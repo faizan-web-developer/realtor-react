a348ec8aee8901ec842c8a8e27f436a153eebab8
geekjaffry <greatjaffry9@gmail.com>
2022-10-25 23:03:24 +0500

    change header profile/sign in button according to the user login status

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import React, { useEffect, useState } from 'react'
 import { useLocation,useNavigate } from 'react-router'
 export default function Header() {
     const location = useLocation();
     const navigate = useNavigate();
+    const [pageStatus, setPageStatus] = useState('Sign in')
+    const auth = getAuth()
+    useEffect(()=>{
+        onAuthStateChanged(auth,(user)=>{
+            if(user){
+                setPageStatus(`Welcome ${user.displayName}`)
+            }else{
+                setPageStatus('Sign In')
+            }
+        })
+    })
     function selectedPath(route){
         if(route === location.pathname){
             return true;
         }
         return false;
     }
+
   return (
     <div className='bg-white border-b shadow-sm sticky top-0 z-50'>
         <header className='flex justify-between items-center px-3 mx-auto max-w-6xl'>
@@ -19,7 +32,7 @@ export default function Header() {
                 <ul className='flex space-x-10'>
                     <li className={`cursor-pointer font-semibold py-3 text-sm text-gray-400 border-b-[3px] border-b-transparent ${selectedPath("/") && "text-black border-b-red-500"}`} onClick={()=>navigate("/")}>Home</li>
                     <li className={`cursor-pointer font-semibold py-3 text-sm text-gray-400 border-b-[3px] border-b-transparent ${selectedPath("/offers") && "text-black border-b-red-500"}`} onClick={()=>navigate("/offers")}>Offers</li>
-                    <li className={`cursor-pointer font-semibold py-3 text-sm text-gray-400 border-b-[3px] border-b-transparent ${selectedPath("/sign-in") && "text-black border-b-red-500"}`} onClick={()=>navigate("/sign-in")}>Sign In</li>
+                    <li className={`cursor-pointer font-semibold py-3 text-sm text-gray-400 border-b-[3px] border-b-transparent ${(selectedPath("/sign-in") || selectedPath("/profile")) && "text-black border-b-red-500"}`} onClick={()=>navigate("/profile")}>{pageStatus}</li>
                 </ul>
             </div>
         </header>
